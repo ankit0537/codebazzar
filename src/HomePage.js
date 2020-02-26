31fbaaf09568c10d8903068501cc69c7e94844a5
ankit kumar <ankitsistecr@gmail.com>
2020-02-26 10:15:37 +0530

    add

---
+++ src/HomePage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomePage=()=>(
+    <div className='homepage'>
+        <div className='categories-container'>
+            <div className='category-item'>
+                <div className='content-holder'>
+                    <h1 className='title'>java full stack</h1>
+                    <span className='subtitle'>know more</span>
+                    </div>
+                         </div>
+
+
+                         <div className='category-item'>
+                <div className='content-holder'>
+                    <h1 className='title'>data science full stack</h1>
+                    <span className='subtitle'>know more</span>
+                    </div>
+                         </div> 
+
+                         <div className='category-item'>
+                <div className='content-holder'>
+                    <h1 className='title'>python full stack</h1>
+                    <span className='subtitle'>know more</span>
+                    </div>
+                         </div> 
+
+                         <div className='category-item'>
+                <div className='content-holder'>
+                    <h1 className='title'> java script full stack</h1>
+                    <span className='subtitle'>know more</span>
+                    </div>
+                         </div> 
+
+
+
+                              </div>
+                                    </div>
+)
+export default HomePage;
